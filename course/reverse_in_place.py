455f13a8900d62067cd9664c822acf58679b7681
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-30 17:27:07 -0400

    complete

--- course/reverse_in_place.py
+++ course/reverse_in_place.py
@@ -1,10 +1,12 @@
 input_string = list("Using a palindrome here would be a bad idea.")
 print(input_string)
 
+
 def rip():
     i = len(input_string)-1
     j = 0
     tmp = 0
+    # This can also be done from the end of the array
     while i > j:
         tmp = input_string[j]
         input_string[j] = input_string[i]
