45e5708b1d6add96109ed9efe9a43f6c70fe0d00
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-30 18:57:09 -0400

    Complete in O(n) time and space

--- course/merge_sorted_arrays.py
+++ course/merge_sorted_arrays.py
@@ -1,10 +1,13 @@
-my_list     = [3, 4, 6, 10, 11, 15]
-alices_list = [1, 5, 8, 12, 14, 19]
+list_a = [3, 4, 6, 10, 11, 15]
+list_b = [1, 5, 8, 12, 14, 19]
 
 def merge_lists(list_a, list_b):
-    list_a.extend(list_b)
-    list_a.sort()
-    print(list_a)
-    return
+    result = []
+    for i in range(len(list_a)):
+        result.append(min(list_a[i], list_b[i]))
+        result.append(max(list_a[i], list_b[i]))
+    if len(list_b) > len(list_a):
+        result.extend(list_b[len(list_a):])
+    print(result)
 
-merge_lists(my_list, alices_list)
+merge_lists(list_a, list_b)
