6d854635ecad03a33525160eaa92795fb4a0dacc
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-30 18:11:12 -0400

    Complete in O(1) space and O(n) time

--- course/reverse_words.py
+++ course/reverse_words.py
@@ -2,23 +2,30 @@ message = [ 'c', 'a', 'k', 'e', ' ',
             'p', 'o', 'u', 'n', 'd', ' ',
             's', 't', 'e', 'a', 'l' ]
 
-def rip(input_string):
-    i = len(input_string)-1
-    j = 0
+def rip(message, j, i):
     tmp = 0
     while i > j:
-        tmp = input_string[j]
-        input_string[j] = input_string[i]
-        input_string[i] = tmp
+        print(i)
+        print(j)
+        tmp = message[j]
+        message[j] = message[i]
+        message[i] = tmp
         i -= 1
         j += 1
-    return input_string
 
-def reverse_words():
-    # Make an list of words not strings
-    tmp_str = ''.join(message)
-    message1 = tmp_str.split(' ')
-    message1 = rip(message1)
-    print(message1)
+def reverse_words(message):
+    # Step 1: reverse the whole thing by characters
+    rip(message, 0, len(message)-1)
+    bottom = 0 # beginning of current word. Prime with the beginning of the array.
+    # Step2: Loop to find individual words
+    for n in range(len(message) + 1):
+        if(n == len(message) or message[n] == ' '):
+            # Step3: Reverse teh word when we find it
+            rip(message, bottom, n-1)
+            # reset bottom
+            bottom = n + 1
+    print(message)
+    #message = rip()
+    #bottom = 0
 
-reverse_words()
+reverse_words(message)
