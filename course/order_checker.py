2bd365e524bf2ea0599b0db05a0fecdb8260d994
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-30 22:12:21 -0400

    complete in O(n) time and O(1) space

--- course/order_checker.py
+++ course/order_checker.py
@@ -1,3 +1,17 @@
 takeout = [1, 3, 5]
 eatin = [2, 4, 6]
 served = [1, 2, 3, 5, 4, 6]
+
+def validate_order(takeout, eatin, served):
+    takout_index 0
+    eatin_index = 0
+
+    for i in served:
+        if i == takout[takout_index]:
+            takout_index += 1
+        elif i == eatin[eatin_index]:
+            eatin_index += 1
+        else:
+            print("You are out of order!")
+            return
+    return "It VERKS!!!"
