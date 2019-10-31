7f5dd7b72a1d42ae5704a59629e897ba9928b1f2
Isaac Friedman <isaac@isaac-friedman.com>
2019-10-31 16:52:31 -0400

    Better solution using a set, O(n) time

--- course/02_chapter2_hashing_and_dictionaries/inflight_entertainment.py
+++ course/02_chapter2_hashing_and_dictionaries/inflight_entertainment.py
@@ -4,10 +4,11 @@ def pair_movies(flight_length, movie_lengths):
     returns "true" if there is a pair of movies exactly as long as the flight
     and false otherwise
     """
-    ttk = 0
-    for i in movie_lengths:
+    ttk = 0 #Time to kill after first movie
+    s = set(movie_lengths) # O(1) lookup time in the average case.
+    for i in movie_lengths: # O(n)
         ttk = flight_length - i
-        if ttk in movie_lengths:
+        if ttk in s: # O(1)
             return True
     return False
 
