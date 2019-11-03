b9ddcc31233aa68db8ad335ddeefe91ed9563117
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-03 12:51:11 -0500

    Complete in O(n) time using a counting sort and no additional data structures

--- course/02_chapter2_hashing_and_dictionaries/top_scores.py
+++ course/02_chapter2_hashing_and_dictionaries/top_scores.py
@@ -1,11 +1,24 @@
-from sortedcontainers import SortedList
+def top_scores(unsorted_scores):
+    HIGHEST_POSSIBLE_SCORE = 100
+    score_counts = [0] * (HIGHEST_POSSIBLE_SCORE + 1)
 
-def top_scores(scores):
-    return SortedList(scores)
+    for score in unsorted_scores:
+        score_counts[score] += 1
+    sorted_scores = []
 
-
+    for score in range(len(score_counts)):
+        # If the score is unique just append it once
+        if score_counts[score] == 1:
+            sorted_scores.append(score)
+        # Otherwise append it COUNT times
+        elif score_counts[score] > 1:
+            [sorted_scores.append(score) for i in range(score_counts[score])]
+        # Pass if count is 0
+        else:
+            pass
+    return sorted_scores
 
 unsorted_scores = [37, 89, 41, 65, 91, 53]
-HIGHEST_POSSIBLE_SCORE = 100
+
 
 print(top_scores(unsorted_scores))
