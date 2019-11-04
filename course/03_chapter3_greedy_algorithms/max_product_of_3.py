eb82821afd44497b445d8f8ee3188e30ed4f3a8f
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-04 18:54:38 -0500

    Naive solution in O(nlgn) but accounts for negatives this time

--- course/03_chapter3_greedy_algorithms/max_product_of_3.py
+++ course/03_chapter3_greedy_algorithms/max_product_of_3.py
@@ -1,8 +1,11 @@
 def max_product_of_3(input_list):
     input_list = sorted(input_list)
-    output_list = input_list[-3:]
-    print(output_list)
-    return "Naive solution"
+    if abs(input_list[0]) > input_list[-1] and abs(input_list[1]) > input_list[-1]:
+        output_list = [input_list[0], input_list[1], input_list[-1]]
+    else:
+        output_list = input_list[-3:]
 
-input_list= [10,10,1,3,2]
-max_product_of_3(input_list)
+    return output_list
+
+input_list= [-10,-10,1,3,2]
+print(max_product_of_3(input_list))
