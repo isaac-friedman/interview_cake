c5f2a0c83e0f4f13583604b96da873fe394165bd
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-04 17:28:42 -0500

    naive solution O(n^2 + kn)

--- course/03_chapter3_greedy_algorithms/apple_stocks.py
+++ course/03_chapter3_greedy_algorithms/apple_stocks.py
@@ -2,11 +2,20 @@
 Problem: Write an efficient function that takes stock_prices and returns the
 best profit I could have made from one purchase and one sale of one share of
 Apple stock yesterday.
+
+Constraint: You have to buy before you can sell
 """
 
 def get_max_profit(stock_prices):
-    # If we were to allow shorting, buying on margin and all of the usual financial shenanigans
-    return(max(stock_prices) - min(stock_prices))
+    current_spread = 0
+    for bottom in range(len(stock_prices)):
+        later = stock_prices[bottom:]
+        print(later)
+        for top in later:
+            if top - stock_prices[bottom] > current_spread:
+                current_spread = top - stock_prices[bottom]
+    return current_spread
+
 
 stock_prices = [10, 7, 5, 8, 11, 9]
-get_max_profit(stock_prices)
+print(get_max_profit(stock_prices))
