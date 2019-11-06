6ff64946d992a9414aaf9b82af704aa6fc0ba697
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-06 18:08:14 -0500

    complete solution in place

--- course/03_chapter3_greedy_algorithms/inplace_shuffle.py
+++ course/03_chapter3_greedy_algorithms/inplace_shuffle.py
@@ -4,20 +4,9 @@ def get_rand(ceiling):
     return randrange(0, ceiling+1)
 
 def inplace_shuffle(input_list):
-    # Value in input_list is the key, new location is the value
-    output_dict = {key:None for key in input_list}
-    got_rand = 0
-    for i in input_list:
-        flag = False
-        while flag == False:
-            new_loc = get_rand(len(input_list))
-            got_rand += 1
-            if new_loc not in output_dict.values():
-                flag = True
-                output_dict[i] = new_loc
-    print(f"This approach led to us calling get_rand {got_rand} times.")
-    print(output_dict)
-    return
-
+    for i in range(len(input_list)-1):
+        new_loc = get_rand(len(input_list))
+        input_list[i], input_list[new_loc] = input_list[new_loc], input_list[i]
+        print(input_list)
 input_list = [11, 31, 21, 51, 41, 71, 61, 91, 81]
 inplace_shuffle(input_list)
