591faf2d80da08ed671516e95a6db7d4f9e3f154
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-05 22:23:46 -0500

    O(n) time using greedy algorithm

--- course/03_chapter3_greedy_algorithms/multiply_other_ints.py
+++ course/03_chapter3_greedy_algorithms/multiply_other_ints.py
@@ -1,13 +1,25 @@
 def product_of_all_other_ints(input_list):
-    output_list = []
-    master_multiple = 1
-    for i in input_list:
-        master_multiple *= i
-    print(master_multiple)
-    for i in input_list:
-        output_list.append(master_multiple/i)
-    return output_list
+    products_before_each = [None] * len(input_list)
+    products_after_each = [None] * len(input_list)
+    products_besides_each = [None] * len(input_list)
+    so_far = 1
+    # Look at the product up to each index
+    for i in range(len(input_list)):
+        so_far *= input_list[i]
+        products_before_each[i] = so_far
 
+    # Reset our temp variable before going backwards
+    so_far = 1
+    for i in range(len(input_list) - 1, -1, -1):
+        so_far *= input_list[i]
+        products_after_each[i] = so_far
+    print(products_before_each)
+    print(products_after_each)
+
+    for i in range(len(input_list)):
+        products_besides_each[i] = products_before_each[i] * products_after_each[i]
+
+    return products_besides_each
 
 
 input_list = [1, 7, 3, 4]
