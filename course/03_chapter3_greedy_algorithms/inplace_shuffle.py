e01e45643cbe1b727c1db5621a6aa03bbbb2c293
Isaac Friedman <isaac@isaac-friedman.com>
2019-11-06 17:58:39 -0500

    All outcomes have equal probability but solution is not in place

--- course/03_chapter3_greedy_algorithms/inplace_shuffle.py
+++ course/03_chapter3_greedy_algorithms/inplace_shuffle.py
@@ -4,8 +4,19 @@ def get_rand(ceiling):
     return randrange(0, ceiling+1)
 
 def inplace_shuffle(input_list):
-    for first_index in range(len(input_list)):
-        second_index = get_rand(len(input_list))
-        input_list[first_index], input_list[second_index] = input_list[second_index], input_list[first_index]
-    print(input_list)
+    output_dict = {key:None for key in input_list}
+    got_rand = 0
+    for i in input_list:
+        flag = False
+        while flag == False:
+            new_loc = get_rand(len(input_list))
+            got_rand += 1
+            if new_loc not in output_dict.values():
+                flag = True
+                output_dict[i] = new_loc
+    print(f"This approach led to us calling get_rand {got_rand} times.")
+    print(output_dict)
     return
+
+input_list = [11, 31, 21, 51, 41, 71, 61, 91, 81]
+inplace_shuffle(input_list)
